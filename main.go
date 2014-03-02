facc3612053321b5bc666a8040935df680746d75
Sheppard Y <ysf1026@gmail.com>
2014-03-02 22:56:02 +0800

    service, test

--- main.go
+++ main.go
@@ -4,9 +4,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/yangsf5/claw/service"
 )
 
 func main() {
+	var services map[string]func()
+	services = make(map[string]func())
+	services["test"] = service.Test
+
+	services["test"]()
+
 	fmt.Println("hell, claw!")
 }
 
---
+++ service/test.go
@@ -0,0 +1,12 @@
+// Author: sheppard(ysf1026@gmail.com) 2014-03-02
+
+package service
+
+import (
+	"fmt"
+)
+
+func Test() {
+	fmt.Println("Service.test")
+}
+
