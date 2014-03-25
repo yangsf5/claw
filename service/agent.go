5a7b91ac48e89cca067a0e6aa97022858e993ceb
Sheppard Y <ysf1026@gmail.com>
2014-03-25 23:55:20 +0800

    init agent

---
+++ service/agent.go
@@ -0,0 +1,12 @@
+// Author: sheppard(ysf1026@gmail.com) 2014-03-25
+
+package service
+
+type Agent struct {
+}
+
+func (s* Agent) ClawCallback(session int, source string, msgType int, msg interface{}) {
+}
+
+func (s* Agent) ClawStart() {
+}
