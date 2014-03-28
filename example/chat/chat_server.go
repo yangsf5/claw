5495500b0c8a965df7eb6ab632f60626df175959
Sheppard Y <ysf1026@gmail.com>
2014-03-28 19:23:41 +0800

    net handler for chat, not complete

--- example/chat/chat_server.go
+++ example/chat/chat_server.go
@@ -8,11 +8,25 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/yangsf5/claw/center"
+	clawNet "github.com/yangsf5/claw/engine/net"
 	"github.com/yangsf5/claw/service"
 )
 
 var (
+	clients *clawNet.Group
 )
+
+type Client struct {
+	conn net.Conn
+}
+
+func (c *Client) Send(msg []byte) {
+	c.conn.Write(msg)
+}
+
+func init() {
+	clients = clawNet.NewGroup
+}
 
 func main() {
 	service.Register()
@@ -30,5 +44,21 @@ func main() {
 }
 
 func connHandle(conn net.Conn) {
-	glog.Info(conn)
+	defer conn.Close()
+	clients.AddPeer(conn.RemoteAddr(), client)
+
+	cb := func(reader *bufio.Reader, err error) {
+		if err != nil {
+			clients.DelPeer(conn.RemoteAddr())
+			glog.Infof("Event: %s leave", conn.RemoteAddr())
+			return
+		}
+
+		n := reader.Buffered()
+		packBuf := make([]byte, n)
+		reader.Read(packBuf)
+		glog.Infof("%s say: %s", conn.RemoteAddr(), string(packBuf))
+	}
+
+	clawNet.RecvLoop(conn, cb)
 }
--- example/chat/client.go
+++ example/chat/client.go
@@ -2,5 +2,37 @@
 
 package main
 
+import (
+	"net"
+	"io/ioutil"
+
+	clawNet "github.com/yangsf5/claw/engine/net"
+)
+
+var (
+	conn net.Conn
+)
+
 func main() {
+}
+
+func connect() {
+	conn, err := net.Dial("tcp", "127.0.0.1:11001")
+	checkError(err)
+	recv()
+}
+
+func send(msg []byte) {
+	_, err := conn.Write(buf)
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func recv() {
+	//TODO clawNet.RecvLoop
 }
